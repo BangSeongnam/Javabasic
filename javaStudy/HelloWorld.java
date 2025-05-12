078b2ac40335692e58a120ec0c3fb8e7a8d269a4
BangSeongnam <fullstack.qkdtjdska12@gmail.com>
2025-05-12 17:44:26 +0900

    init commit

---
+++ javaStudy/HelloWorld.java
@@ -0,0 +1,11 @@
+package javaStudy;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕. 난 방성남 이야.");
+		
+	}
+
+}
